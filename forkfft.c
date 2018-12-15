590a43f6669760a3ce013bdaed653dac9e0efa92
robaerd <robert.sowula9879@gmail.com>
2018-12-15 16:30:59 +0100

    added error handling

--- forkfft.c
+++ forkfft.c
@@ -17,6 +17,8 @@
 
 static void order (_Complex float *, int, _Complex float **, _Complex float **);
 
+static void parseWaitPID(pid_t, int);
+
 int main(int argc, char **argv)
 {
   //check if arguments are provided. if so terminate with exit code EXIT_FAILURE
@@ -73,86 +75,121 @@ int main(int argc, char **argv)
   pid_t pid_E = fork();
 
   if (pid_E == -1) {
-      fprintf(stderr, "error during even Fork");
+    fprintf(stderr, "error during even fork");
+    free(P_E);
+    free(P_O);
+    exit(EXIT_FAILURE);
+  } else if (pid_E > 0) {
+    close(pipefd2[0]); // closes reading side from parent. only need write end
+    writeToFD(P_E, actualValues/2, pipefd2[1]);
+    close(pipefd2[1]); //error handling
+    close(pipefd1[1]); // close writing end of p1
+
+    //fork with even indices
+    pid_t pid_O = fork();
+
+    if (pid_O == -1) {
+      fprintf(stderr, "error during odd fork");
       free(P_E);
       free(P_O);
       exit(EXIT_FAILURE);
-	} else if (pid_E > 0) {
-      close(pipefd2[0]); // closes reading side from parent. only need write end
-      writeToFD(P_E, actualValues/2, pipefd2[1]);
-      close(pipefd2[1]); //error handling
-      close(pipefd1[1]); // close writing end of p1
+    } else if (pid_O > 0) {
+      // write even array to second child
+      close(pipefd4[0]); // closes reading side from parent. only need write end
+      writeToFD(P_O, actualValues/2, pipefd4[1]); //
+      close(pipefd4[1]); //error handling
+      close(pipefd3[1]); // close writing end of p1
 
-      //fork with even indices
-      pid_t pid_O = fork();
-
-      if (pid_O == -1) {
-          fprintf(stderr, "error during odd Fork");
-          free(P_E);
-          free(P_O);
-          exit(EXIT_FAILURE);
-		} else if (pid_O > 0) {
-          // write even array to second child
-          close(pipefd4[0]); // closes reading side from parent. only need write end
-          writeToFD(P_O, actualValues/2, pipefd4[1]); //
-          close(pipefd4[1]); //error handling
-          close(pipefd3[1]); // close writing end of p1
-		  //wait and receive result
-          int status2;
-          waitpid(pid_O, &status2, 0);
-		  readFdIn(P_O, pipefd3[0]);
-          close(pipefd3[0]);
-		} else {
-          free(P_E);
-          free(P_O);
-          if(dup2(pipefd3[1], STDOUT_FILENO) == -1) {
-            fprintf(stderr, "Error duplicating STDOUT to write end of third pipe: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
+      int status2;
+      pid_t waitStatus2;
+      do {
+        waitStatus2 = waitpid(pid_O, &status2, 0);
+        if (waitStatus2 == -1) {
+            if (errno == ECHILD)
+              fprintf(stderr,"ECHILD ERROR:  %s\n", strerror(errno));
+            else if (errno == EINTR)
+              fprintf(stderr,"EINTR ERROR: %s\n", strerror(errno));
+            else
+              fprintf(stderr,"EINTR ERROR: %s\n", strerror(errno));
           }
-          close(pipefd3[1]);
-          close(pipefd3[0]);
 
-          if (dup2(pipefd4[0], STDIN_FILENO) == -1) {
-            fprintf(stderr, "Error duplicating STDIN to read end of fourth pipe: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-          }
-          close(pipefd4[0]);
-          close(pipefd4[1]);
+        if (WEXITSTATUS(status2) == EXIT_FAILURE)
+          fprintf(stderr,"exited with failure\n");
 
-          execlp("./forkfft", "forkfft", NULL);
-          //if execlp not succesfull
-          fprintf(stderr, "exec error on odd part");
-          exit(EXIT_FAILURE);
-		}
-      //wait and receive result
-      int status1;
-      waitpid(pid_E, &status1, 0);
+      } while (!WIFEXITED(status2));
 
-      readFdIn(P_E, pipefd1[0]);
-      close(pipefd1[0]);
-	} else {
+
+
+
+      readFdIn(P_O, pipefd3[0]);
+      close(pipefd3[0]);
+    } else {
       free(P_E);
       free(P_O);
-      if (dup2(pipefd1[1], STDOUT_FILENO) == -1) {
-        fprintf(stderr, "Error duplicating STDOUT to write end of first pipe: %s\n", strerror(errno));
+      if(dup2(pipefd3[1], STDOUT_FILENO) == -1) {
+        fprintf(stderr, "Error duplicating STDOUT to write end of third pipe: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
       }
-      close(pipefd1[1]);
-      close(pipefd1[0]);
+      close(pipefd3[1]);
+      close(pipefd3[0]);
 
-      if(dup2(pipefd2[0], STDIN_FILENO) == -1) {
-        fprintf(stderr, "Error duplicating STDIN to read end of second pipe: %s\n", strerror(errno));
+      if (dup2(pipefd4[0], STDIN_FILENO) == -1) {
+        fprintf(stderr, "Error duplicating STDIN to read end of fourth pipe: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
       }
-      close(pipefd2[0]);
-      close(pipefd2[1]);
-      close(pipefd2[0]);
+      close(pipefd4[0]);
+      close(pipefd4[1]);
 
       execlp("./forkfft", "forkfft", NULL);
-      //if execlp succesfull, next lines will not be reached
-      fprintf(stderr, "exec error on even part");
+      //if execlp not succesfull
+      fprintf(stderr, "exec error on odd part");
       exit(EXIT_FAILURE);
-	}
+    }
+
+    int status1;
+    pid_t waitStatus1;
+    do {
+      waitStatus1 = waitpid(pid_E, &status1, 0);
+      if (waitStatus1 == -1) {
+        if (errno == ECHILD)
+          fprintf(stderr,"ECHILD ERROR:  %s\n", strerror(errno));
+        else if (errno == EINTR)
+          fprintf(stderr,"EINTR ERROR: %s\n", strerror(errno));
+        else
+          fprintf(stderr,"EINTR ERROR: %s\n", strerror(errno));
+      }
+
+      if (WEXITSTATUS(status1) == EXIT_FAILURE)
+        fprintf(stderr,"exited with failure\n");
+
+    } while (!WIFEXITED(status1));
+
+
+    readFdIn(P_E, pipefd1[0]);
+    close(pipefd1[0]);
+  } else {
+    free(P_E);
+    free(P_O);
+    if (dup2(pipefd1[1], STDOUT_FILENO) == -1) {
+      fprintf(stderr, "Error duplicating STDOUT to write end of first pipe: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    close(pipefd1[1]);
+    close(pipefd1[0]);
+
+    if(dup2(pipefd2[0], STDIN_FILENO) == -1) {
+      fprintf(stderr, "Error duplicating STDIN to read end of second pipe: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    close(pipefd2[0]);
+    close(pipefd2[1]);
+    close(pipefd2[0]);
+
+    execlp("./forkfft", "forkfft", NULL);
+    //if execlp succesfull, next lines will not be reached
+    fprintf(stderr, "exec error on even part");
+    exit(EXIT_FAILURE);
+  }
 
   //calculate DFT
   _Complex float *R = (_Complex float *)malloc(sizeof(_Complex float) * actualValues);
@@ -178,4 +215,23 @@ void order(_Complex float *EandO, int EandO_length,_Complex float **E,_Complex f
     *(*E+i) = EandO[i*2];
     *(*O+i) = EandO[(i*2+1)];
   }
+}
+
+void parseWaitPID(pid_t waitpidReturnVal,int status){
+  if (waitpidReturnVal > 0) {
+    if (WIFEXITED(status))
+      fprintf(stderr,"Exit Code: %d\n", WEXITSTATUS(status));
+    else
+      fprintf(stderr,"Exit Status: 0x%.4X\n", status);
+  } else if (waitpidReturnVal == 0)
+    fprintf(stderr,"Child process still running\n");
+  else {
+    if (errno == ECHILD)
+      fprintf(stderr," Error ECHILD!!\n");
+    else if (errno == EINTR)
+      fprintf(stderr," Error EINTR!!\n");
+    else
+      fprintf(stderr,"Error EINVAL!!\n");
+  }
+
 }
